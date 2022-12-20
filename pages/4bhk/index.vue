12df602e4e0a45d42233daf035e78c0438f24186
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2022-12-20 11:48:50 +0100

    added preset wind preset to package.json

---
+++ pages/4bhk/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-main>
+    <v-container>
+      <v-btn>Simple Button</v-btn>
+      <v-btn color="error">Error-Button</v-btn>
+      <v-card>
+        <v-card-title>Card-Title</v-card-title>
+        <v-card-subtitle>Card-Subtitle</v-card-subtitle>
+        <v-card-text>Card-Text</v-card-text>
+        <v-card-actions>
+          <v-btn color="primary">Primary-Button</v-btn>
+          <v-btn color="secondary">Secondary-Button</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-container>
+  </v-main>
+</template>
